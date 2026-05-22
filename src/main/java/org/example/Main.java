31074c8bc1fa360299d7de9e869d69aa30816e4b
Corey <codeancrazy9@gmail.com>
2026-05-22 15:25:51 -0400

    ui

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import UI.UserInterface;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //Build
+        UserInterface ui = new UserInterface();
 
     }
 }
