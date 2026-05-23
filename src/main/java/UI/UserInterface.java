81b3c90c9e08d6f48b64dc52aaf81e48dcd4cb68
Corey <codeancrazy9@gmail.com>
2026-05-23 10:20:00 -0400

    quest menu. method placeholders

--- src/main/java/UI/UserInterface.java
+++ src/main/java/UI/UserInterface.java
@@ -20,7 +20,7 @@ public class UserInterface {
             scanner.nextLine();
             switch(choice){
                 case 1:
-                    displayHomeScreen();
+                    displayOrderScreen();
                 case 0:
                     running = false;
                     System.out.println("\nThis journey is not for you, safe travels!");
@@ -29,5 +29,42 @@ public class UserInterface {
                     System.out.println("You must have had too much ale, try again!");
             }
         }
+    }
+
+    public void displayOrderScreen(){
+        boolean ordering = true;
+        while(ordering){
+            //art menu TBD
+            Art.displayQuestMenu();
+
+            System.out.println("1. Forge your loadout: (add sandwich)");
+            System.out.println("2.) Brew potions (add drinks)");
+            System.out.println("3.) Purchase Rations (add chips)");
+            System.out.println("4.) Start journey (checkout");
+            System.out.println("0. Abandon destiny (cancel order)");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch(choice){
+                case 1:
+                    addSandwich();
+                    break;
+                case 2:
+                    brewPotions();
+                    break;
+                case 3:
+                    purchaseRations();
+                    break;
+                case 4:
+                    startJourney();
+                    break;
+                case 0:
+                    System.out.println("\nDestiny has yet to call this one..");
+                default:
+                    System.out.println("\n.. too much ale again huh?");
+            }
+        }
+    }
+    public void addSandwich(){
+        System.out.println();
     }
 }
