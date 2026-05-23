18b2e17e8c86fbf98524c7e2eeb97d7622c0c0f5
Corey <codeancrazy9@gmail.com>
2026-05-23 10:03:15 -0400

    displayHomeScreen(UserInterface)

--- src/main/java/UI/UserInterface.java
+++ src/main/java/UI/UserInterface.java
@@ -1,6 +1,33 @@
 package UI;
 
+import java.util.Scanner;
+
 public class UserInterface {
+    //SCANNER
+    private Scanner scanner = new Scanner(System.in);
     //userInput
     //menus
+    public void displayHomeScreen(){
+        boolean running = true;
+        while(running){
+            //show menu
+            System.out.println("Character builder");
+            System.out.println("1.) Start a new adventure!");
+            System.out.println("0.) Leave Tavern");
+            System.out.println("\nWhat will it be?");
+            int choice = scanner.nextInt();
+            //int be acting up
+            scanner.nextLine();
+            switch(choice){
+                case 1:
+                    displayHomeScreen();
+                case 0:
+                    running = false;
+                    System.out.println("\nThis journey is not for you, safe travels!");
+                    break;
+                default:
+                    System.out.println("You must have had too much ale, try again!");
+            }
+        }
+    }
 }
