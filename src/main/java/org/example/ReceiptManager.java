8739b69f57cc1eed511d6ddd58509ffd5cb5ed75
Corey <codeancrazy9@gmail.com>
2026-05-28 08:23:06 -0400

    receipts txt setup, edits to ReceiptManager

--- src/main/java/org/example/ReceiptManager.java
+++ src/main/java/org/example/ReceiptManager.java
@@ -12,11 +12,14 @@ public class ReceiptManager {
     public void saveReceipt(Order order) {
 
         try {
+            //time
             LocalDateTime now = LocalDateTime.now();
             DateTimeFormatter formatter =
                     DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+            //file path
             String fileName =
                     now.format(formatter) + ".txt";
+
             FileWriter writer =
                     new FileWriter("receipts/" + fileName);
             writer.write("""
@@ -36,7 +39,9 @@ public class ReceiptManager {
                     "TOTAL: $%.2f",
                     order.getOrderTotal()
             ));
+            writer.write("------------------\n");
             writer.close();
+            System.out.println("Receipt saved successfully!");
         } catch (IOException e) {
 
             System.out.println("Error saving receipt.");
